072295479a55e7ad1c88d541305dbb0affcffec9
msjemenblc <msjemenblc@gmail.com>
2024-11-16 13:11:48 +0100

    unit and integration tests on login and register components

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -6,37 +6,201 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { throwError } from 'rxjs';
 import { expect } from '@jest/globals';
+import { AuthService } from '../../services/auth.service';
 import { SessionService } from 'src/app/services/session.service';
-
+import { NgZone } from '@angular/core';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+	let component: LoginComponent;
+	let fixture: ComponentFixture<LoginComponent>;
+	let authService: AuthService;
+	let sessionService: SessionService;
+	let router: Router;
+	let ngZone: NgZone;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LoginComponent],
-      providers: [SessionService],
-      imports: [
-        RouterTestingModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        MatCardModule,
-        MatIconModule,
-        MatFormFieldModule,
-        MatInputModule,
-        ReactiveFormsModule]
-    })
-      .compileComponents();
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [LoginComponent],
+			providers: [SessionService, AuthService],
+			imports: [
+				RouterTestingModule.withRoutes([
+					{ path: 'sessions', component: LoginComponent }
+				]),
+				BrowserAnimationsModule,
+				HttpClientModule,
+				MatCardModule,
+				MatIconModule,
+				MatFormFieldModule,
+				MatInputModule,
+				ReactiveFormsModule,
+			],
+		}).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture = TestBed.createComponent(LoginComponent);
+		component = fixture.componentInstance;
+		authService = TestBed.inject(AuthService);
+		sessionService = TestBed.inject(SessionService);
+		router = TestBed.inject(Router);
+		ngZone = TestBed.inject(NgZone);
+		fixture.detectChanges();
+	});
+
+	describe('Unit Test Suites', () => {
+		it('should create', () => {
+			expect(component).toBeTruthy();
+		});
+
+		it('should mark email as invalid if empty', () => {
+			const emailInput = component.form.get('email');
+
+			emailInput?.setValue('');
+			expect(emailInput?.valid).toBeFalsy();
+			expect(emailInput?.errors?.['required']).toBeTruthy();
+		});
+
+		it('should mark password as invalid if empty', () => {
+			const passwordInput = component.form.get('password');
+
+			passwordInput?.setValue('');
+			expect(passwordInput?.valid).toBeFalsy();
+			expect(passwordInput?.errors?.['required']).toBeTruthy();
+		});
+
+		it('should invalidate email when format is incorrect', () => {
+			const emailInput = component.form.get('email');
+
+			// sans '@'
+			emailInput?.setValue('test.com');
+			fixture.detectChanges();
+			expect(emailInput?.valid).toBeFalsy();
+			expect(emailInput?.hasError('email')).toBeTruthy();
+
+			// caractères invalides
+			emailInput?.setValue('test@g....com');
+			fixture.detectChanges();
+			expect(emailInput?.valid).toBeFalsy();
+			expect(emailInput?.hasError('email')).toBeTruthy();
+		});
+
+		it('should disable submit button if form is invalid', () => {
+			const emailInput = component.form.get('email');
+			const passwordInput = component.form.get('password');
+
+			emailInput?.setValue('');
+			passwordInput?.setValue('');
+			fixture.detectChanges();
+
+			const submitButton = fixture.nativeElement.querySelector(
+				'button[type="submit"]'
+			);
+			expect(submitButton.disabled).toBeTruthy();
+		});
+
+		it('should enable submit button if form is valid', () => {
+			const emailInput = component.form.get('email');
+			const passwordInput = component.form.get('password');
+
+			emailInput?.setValue('test@gmail.com');
+			passwordInput?.setValue('password');
+			fixture.detectChanges();
+
+			const submitButton = fixture.nativeElement.querySelector(
+				'button[type="submit"]'
+			);
+			expect(submitButton.disabled).toBeFalsy();
+		});
+	});
+
+	describe('Integration Test Suites', () => {
+		beforeEach(() => {
+			jest.spyOn(authService, 'login').mockReturnValue(
+				of({
+					token: 'fake-jwt-token',
+					type: 'user',
+					id: 1,
+					username: 'fakeUser',
+					firstName: 'Test',
+					lastName: 'Test',
+					admin: false
+				})
+			);
+		});
+
+		it('should call AuthService.login with correct data on form submission', () => {
+			const emailInput = component.form.get('email');
+			const passwordInput = component.form.get('password');
+
+			emailInput?.setValue('test@gmail.com');
+			passwordInput?.setValue('password');
+
+			ngZone.run(() => {
+				component.submit();
+			})
+
+			expect(authService.login).toHaveBeenLastCalledWith({ email: 'test@gmail.com', password: 'password' });
+		});
+
+		it('should call SessionService.logIn with correct data after succesful login', () => {
+			jest.spyOn(sessionService, 'logIn');
+
+			const emailInput = component.form.get('email');
+			const passwordInput = component.form.get('password');
+
+			emailInput?.setValue('test@gmail.com');
+			passwordInput?.setValue('password');
+
+			ngZone.run(() => {
+				component.submit();
+			});
+
+			expect(sessionService.logIn).toHaveBeenLastCalledWith({
+				token: 'fake-jwt-token',
+				type: 'user',
+				id: 1,
+				username: 'fakeUser',
+				firstName: 'Test',
+				lastName: 'Test',
+				admin: false
+			})
+		});
+
+		it('should redirect to /sessions after successful login', () => {
+			jest.spyOn(router, 'navigate');
+
+			const emailInput = component.form.get('email');
+			const passwordInput = component.form.get('password');
+
+			emailInput?.setValue('test@gmail.com');
+			passwordInput?.setValue('password');
+
+			ngZone.run(() => {
+				component.submit();
+			});
+
+			expect(router.navigate).toHaveBeenLastCalledWith(['/sessions']);
+		});
+
+		it('should set onError to true when login fails', () => {
+			const errorResponse = { message: 'Login failed' };
+			jest.spyOn(authService, 'login').mockReturnValue(throwError(() => new Error(errorResponse.message)));
+
+			const emailInput = component.form.get('email');
+			const passwordInput = component.form.get('password');
+
+			emailInput?.setValue('test@gmail.com');
+			passwordInput?.setValue('password');
+
+			ngZone.run(() => {
+				component.submit();
+			});
+			
+			expect(component.onError).toBeTruthy();
+		});
+	});
 });
--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -6,35 +6,205 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { throwError } from 'rxjs';
 import { expect } from '@jest/globals';
-
+import { AuthService } from '../../services/auth.service';
+import { NgZone } from '@angular/core';
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
+    let component: RegisterComponent;
+    let fixture: ComponentFixture<RegisterComponent>;
+    let authService: AuthService;
+    let router: Router;
+    let ngZone: NgZone;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [RegisterComponent],
-      imports: [
-        BrowserAnimationsModule,
-        HttpClientModule,
-        ReactiveFormsModule,  
-        MatCardModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule
-      ]
-    })
-      .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [RegisterComponent],
+            providers: [AuthService],
+            imports: [
+                RouterTestingModule.withRoutes([
+                    { path: 'login', component: RegisterComponent }
+                ]),
+                BrowserAnimationsModule,
+                HttpClientModule,
+                ReactiveFormsModule,  
+                MatCardModule,
+                MatFormFieldModule,
+                MatIconModule,
+                MatInputModule
+            ]
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(RegisterComponent);
+        component = fixture.componentInstance;
+        authService = TestBed.inject(AuthService);
+        router = TestBed.inject(Router);
+        ngZone = TestBed.inject(NgZone);
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    describe('Unit Test Suites', () => {
+        it('should create', () => {
+            expect(component).toBeTruthy();
+        });
+
+        it('should mark first name as invalid if empty', () => {
+            const firstNameInput = component.form.get('firstName');
+
+            firstNameInput?.setValue('');
+            expect(firstNameInput?.valid).toBeFalsy();
+            expect(firstNameInput?.errors?.['required']).toBeTruthy();
+        });
+
+        it('should mark last name as invalid if empty', () => {
+            const lastNameInput = component.form.get('lastName');
+
+            lastNameInput?.setValue('');
+            expect(lastNameInput?.valid).toBeFalsy();
+            expect(lastNameInput?.errors?.['required']).toBeTruthy();
+        });
+
+        it('should mark email as invalid if empty', () => {
+            const emailInput = component.form.get('email');
+
+            emailInput?.setValue('');
+            expect(emailInput?.valid).toBeFalsy();
+            expect(emailInput?.errors?.['required']).toBeTruthy();
+        });
+
+        it('should mark password as invalid if empty', () => {
+            const passwordInput = component.form.get('password');
+
+            passwordInput?.setValue('');
+            expect(passwordInput?.valid).toBeFalsy();
+            expect(passwordInput?.errors?.['required']).toBeTruthy();
+        });
+        
+		it('should invalidate email when format is incorrect', () => {
+			const emailInput = component.form.get('email');
+
+			// sans '@'
+			emailInput?.setValue('test.com');
+			fixture.detectChanges();
+			expect(emailInput?.valid).toBeFalsy();
+			expect(emailInput?.hasError('email')).toBeTruthy();
+
+			// caractères invalides
+			emailInput?.setValue('test@g....com');
+			fixture.detectChanges();
+			expect(emailInput?.valid).toBeFalsy();
+			expect(emailInput?.hasError('email')).toBeTruthy();
+		});
+
+        it('should disable submit button if form is invalid', () => {
+            const firstNameInput = component.form.get('firstName');
+            const lastNameInput = component.form.get('lastName');
+			const emailInput = component.form.get('email');
+			const passwordInput = component.form.get('password');
+
+            firstNameInput?.setValue('');
+            lastNameInput?.setValue('');
+			emailInput?.setValue('');
+			passwordInput?.setValue('');
+			fixture.detectChanges();
+
+			const submitButton = fixture.nativeElement.querySelector(
+				'button[type="submit"]'
+			);
+			expect(submitButton.disabled).toBeTruthy();
+		});
+
+        it('should enable submit button if form is valid', () => {
+            const firstNameInput = component.form.get('firstName');
+            const lastNameInput = component.form.get('lastName');            
+			const emailInput = component.form.get('email');
+			const passwordInput = component.form.get('password');
+
+            firstNameInput?.setValue('FirstTest');
+            lastNameInput?.setValue('LastTest');
+			emailInput?.setValue('test@gmail.com');
+			passwordInput?.setValue('password');
+			fixture.detectChanges();
+
+			const submitButton = fixture.nativeElement.querySelector(
+				'button[type="submit"]'
+			);
+			expect(submitButton.disabled).toBeFalsy();
+		});
+    });
+
+    describe('Integration Test Suites', () => {
+        beforeEach(() => {
+            jest.spyOn(authService, 'register').mockReturnValue(of(void 0));
+        });
+
+        it('should call AuthService.register with correct data on form submission', () => {
+            const firstNameInput = component.form.get('firstName');
+            const lastNameInput = component.form.get('lastName');            
+			const emailInput = component.form.get('email');
+			const passwordInput = component.form.get('password');
+        
+            firstNameInput?.setValue('FirstTest');
+            lastNameInput?.setValue('LastTest');
+            emailInput?.setValue('test@gmail.com');
+			passwordInput?.setValue('password');
+
+            ngZone.run(() => {
+				component.submit();
+			});
+
+            expect(authService.register).toHaveBeenLastCalledWith({
+                firstName: 'FirstTest',
+                lastName: 'LastTest',
+                email: 'test@gmail.com',
+                password: 'password'
+            });
+        });
+
+        it('should redirect to /login after successful registration', () => {
+            jest.spyOn(router, 'navigate');
+
+            const firstNameInput = component.form.get('firstName');
+            const lastNameInput = component.form.get('lastName');            
+			const emailInput = component.form.get('email');
+			const passwordInput = component.form.get('password');
+            
+            firstNameInput?.setValue('FirstTest');
+            lastNameInput?.setValue('LastTest');
+            emailInput?.setValue('test@gmail.com');
+			passwordInput?.setValue('password');
+
+            ngZone.run(() => {
+				component.submit();
+			});
+
+			expect(router.navigate).toHaveBeenLastCalledWith(['/login']);
+        });
+
+        it('should set onError to true when registration fails', () => {
+            const errorResponse = { message: 'Registration failed' };
+            jest.spyOn(authService, 'register').mockReturnValue(throwError(() => new Error(errorResponse.message))); 
+        
+            const firstNameInput = component.form.get('firstName');
+            const lastNameInput = component.form.get('lastName');            
+			const emailInput = component.form.get('email');
+			const passwordInput = component.form.get('password');
+            
+            firstNameInput?.setValue('FirstTest');
+            lastNameInput?.setValue('LastTest');
+            emailInput?.setValue('test@gmail.com');
+			passwordInput?.setValue('password');
+
+            ngZone.run(() => {
+				component.submit();
+			});
+			
+			expect(component.onError).toBeTruthy();
+        });
+    });
 });
