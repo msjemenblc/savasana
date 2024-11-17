0b17a144a2c24f30ca9078ca8005c5cc31fdb879
msjemenblc <msjemenblc@gmail.com>
2024-11-17 23:30:48 +0100

    unit tests on NotFoundComponent

--- front/src/app/components/not-found/not-found.component.spec.ts
+++ front/src/app/components/not-found/not-found.component.spec.ts
@@ -1,24 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { expect } from '@jest/globals';
 
 import { NotFoundComponent } from './not-found.component';
 
 describe('NotFoundComponent', () => {
-  let component: NotFoundComponent;
-  let fixture: ComponentFixture<NotFoundComponent>;
+	let component: NotFoundComponent;
+	let fixture: ComponentFixture<NotFoundComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NotFoundComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [ NotFoundComponent ]
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(NotFoundComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(NotFoundComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	describe('Unit Test Suites', () => {
+		it('should create', () => {
+			expect(component).toBeTruthy();
+		});
+
+		it('should display the correct message', () => {
+			const element = fixture.debugElement.query(By.css('h1'));
+
+			expect(element).toBeTruthy();
+			expect(element.nativeElement.textContent.trim()).toBe('Page not found !');
+		});
+	});
 });
