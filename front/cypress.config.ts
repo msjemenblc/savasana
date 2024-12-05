de7f367210115f100bf25f89ed7b137bb2d354a4
msjemenblc <msjemenblc@gmail.com>
2024-12-05 12:51:53 +0100

    end2end tests completed

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: 'qcxci3',
   videosFolder: 'cypress/videos',
   screenshotsFolder: 'cypress/screenshots',
   fixturesFolder: 'cypress/fixtures',
--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,27 +1,45 @@
+/// <reference types="cypress" />
+
 describe('Login spec', () => {
-  it('Login successfull', () => {
-    cy.visit('/login')
+	it('should visit sessions page when login is successful', () => {
+		cy.visit('/login')
 
-    cy.intercept('POST', '/api/auth/login', {
-      body: {
-        id: 1,
-        username: 'userName',
-        firstName: 'firstName',
-        lastName: 'lastName',
-        admin: true
-      },
-    })
+		cy.intercept('POST', '/api/auth/login', {
+			body: {
+				id: 1,
+				username: 'userName',
+				firstName: 'firstName',
+				lastName: 'lastName',
+				admin: true
+			},
+		})
 
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/api/session',
-      },
-      []).as('session')
+		cy.intercept(
+		{
+			method: 'GET',
+			url: '/api/session',
+		},
+		[]).as('session')
 
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+		cy.get('input[formControlName=email]').type("yoga@studio.com")
+		cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
-    cy.url().should('include', '/sessions')
-  })
+		cy.url().should('include', '/sessions')
+	})
+
+	it('should show error message when login fails', () => {
+		cy.visit('/login')
+
+		cy.intercept('POST', '/api/auth/login', {
+			statusCode: 401,
+			body: { message: 'Bad credentials' }
+		})
+
+		cy.get('input[formControlName=email]').type("fail@studio.com")
+		cy.get('input[formControlName=password]').type(`${"fail!1234"}{enter}{enter}`)
+
+		cy.url().should('not.include', '/sessions')
+
+		cy.get('p.error').should('contain', 'An error occurred')
+	})
 });
